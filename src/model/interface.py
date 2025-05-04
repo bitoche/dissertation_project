fc57fab5d84c3dbb2ed43697c2f6f692adcdb442
Schelkov Andrey <andrey.shchelkov@rtl-consulting.ru>
2025-05-04 12:42:41 +0300

    rewrite interface model and readme

--- src/model/interface.py
+++ src/model/interface.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel
 
 class StartReportItem(BaseModel):
-    calc_id: int
-    report_date: str
-    prev_report_date: str = None
-    actual_date: str = None
+    calc_id: int = 111
+    report_date: str = ''
+    actual_date: str = ''
+    prev_report_date: str = ''
     type: str = 'start_report_item'
 
